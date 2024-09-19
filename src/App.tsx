aadd45b6427b4d6da3bfaaaf4ab649760a808d3d
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-09-19 12:02:45 +0200

    Added the initial set of tasks in the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import AddTaskForm from "./components/AddTaskForm.tsx";
 import Task from "./components/Task.tsx";
 import './App.css'
+import React, {useState} from "react";
 
-const App = () => {
+interface TaskProps {
+
+}
+
+const App: React.FC = () => {
+
+    const [tasks, setTasks] = useState<TaskElement[]> ([
+        {id: "1", task: "Water the plants"},
+        {id: "2", task: "Read a book"},
+        {id: "3", task: "Find a good pasta recipe"},
+    ]);
+
+
 };
 
 export default App
