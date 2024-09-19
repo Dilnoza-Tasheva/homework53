8455d9032f726cc156defa4a5d5ca3bd5b46d0c9
Dilnoza Tasheva <dilnozatasheva@gmail.com>
2024-09-19 11:56:34 +0200

    Added input element to the AddTaskForm.tsx

--- src/components/AddTaskForm.tsx
+++ src/components/AddTaskForm.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 
 interface AddTaskFormProps {
+    currentTask: string;
     addTask: () => void;
 }
 
 const AddTaskForm: React.FC<AddTaskFormProps> = ({addTask}) => {
     return (
-        <button type={"button"} onClick={addTask}>Add</button>
+        <div className="add-task-btn">
+            <input type="text" placeholder="Add a new task" value={currentTask}/>
+            <button type={"button"} onClick={addTask}>Add</button>
+        </div>
+
     );
 };
 
